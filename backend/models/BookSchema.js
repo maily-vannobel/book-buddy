4b0b062626052f77649c872fd06a54b148145d05
maily-vannobel <maily.vannobel@laplateforme.io>
2024-05-22 18:34:16 +0200

    création structure du projet

---
+++ backend/models/BookSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    image: String,
+    status: { type: String, required: true },
+    totalPages: { type: Number, required: true },
+    currentPage: { type: Number, default: 0 },
+    category: String
+});
+
+module.exports = mongoose.model('Book', bookSchema, 'books');
