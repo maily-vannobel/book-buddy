21b315c05c9c6aa15fe4d63e523fa665f0153756
alonzo-bachelier <alonzo.bachelier@laplateforme.io>
2024-05-24 11:44:30 +0200

    Ajout de usersController qui contient les fonction que les routes utilisent

--- backend/routes/bookRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const bookController = require('../controllers/bookController');
-
-const router = express.Router();
-
-// Route GET pour récupérer tous les livres
-router.get('/books', bookController.getAllBooks);
-
-module.exports = router;
