6d7db58617f789df1d0eb72ace70d999332cedcb
alonzo-bachelier <alonzo.bachelier@laplateforme.io>
2024-05-27 10:16:36 +0200

    MAJ lien du fetch de getUserInfo

--- api/fetch/usersFetch.js
+++ api/fetch/usersFetch.js
@@ -19,28 +19,31 @@ async function fetchWithAuth(url, options = {}) {
 
     return response.json();
 }
-
-async function updatePassword(oldPassword, newPassword) {
+async function register(username, email, password) {
     try {
-        const data = await fetchWithAuth(
-            "http://localhost:8080/api/user/updatePassword",
+        const response = await fetch(
+            "http://localhost:8080/api/user/register",
             {
-                method: "PUT",
-                body: JSON.stringify({ oldPassword, newPassword }),
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, email, password }),
             }
         );
-        console.log("Mot de passe mis à jour avec succès", data);
+        const data = await response.json();
+        if (response.ok) {
+            console.log("Enregistrement réussi");
+        } else {
+            console.error("Erreur lors de l'enregistrement:", data.message);
+        }
     } catch (error) {
-        console.error(
-            "Erreur lors de la mise à jour du mot de passe:",
-            error.message
-        );
+        console.error("Erreur lors de l'enregistrement:", error);
     }
 }
-
 async function login(username, password) {
     try {
-        const response = await fetch("http://localhost:8080/api/user/login", {
+        const response = await fetch("http://localhost:3000/api/user/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -56,5 +59,46 @@ async function login(username, password) {
         }
     } catch (error) {
         console.error("Erreur lors de la connexion:", error);
+    }
+}
+function logout() {
+    localStorage.removeItem("token");
+    console.log("Déconnexion réussie");
+}
+async function updatePassword(oldPassword, newPassword) {
+    try {
+        const data = await fetchWithAuth(
+            "http://localhost:3000/api/user/updatePassword",
+            {
+                method: "PUT",
+                body: JSON.stringify({ oldPassword, newPassword }),
+            }
+        );
+        console.log("Mot de passe mis à jour avec succès", data);
+    } catch (error) {
+        console.error(
+            "Erreur lors de la mise à jour du mot de passe:",
+            error.message
+        );
+    }
+}
+async function getUserInfo() {
+    try {
+        const data = await fetchWithAuth(
+            "http://localhost:8080/api/user/users",
+            {
+                method: "GET",
+            }
+        );
+        console.log(
+            "Informations de l'utilisateur récupérées avec succès",
+            data
+        );
+        return data;
+    } catch (error) {
+        console.error(
+            "Erreur lors de la récupération des informations de l'utilisateur:",
+            error.message
+        );
     }
 }
