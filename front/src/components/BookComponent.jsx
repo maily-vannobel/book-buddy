9fe65244e7279cf24f2a12c7514ba9afbc67e6ee
maily-vannobel <maily.vannobel@laplateforme.io>
2024-05-31 23:32:27 +0200

    ajout nav + boostrap

--- front/src/components/BookComponent.jsx
+++ front/src/components/BookComponent.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Modal from 'react-modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/bookComponent.css';
 
 Modal.setAppElement('#root');
@@ -40,54 +41,74 @@ function BookComponent({ book, onFavoriteToggle, onUpdateBook }) {
     }
 
     return (
-        <div className="book-component" onClick={openModal}>
-            <h3 className="book-title">{book.titre}</h3>
-            <p className="book-author">{book.auteur}</p>
-            <p className="book-state">État: {book.etat}</p> {/* Affichage de l'état du livre */}
-            {book.image && <img className="book-image" src={book.image} alt={`${book.titre} cover`} />}
-            <button className="favorite-button" onClick={(e) => { e.stopPropagation(); onFavoriteToggle(book) }}>
-                {book.favori ? "Retirer des favoris" : "Ajouter aux favoris"}
-            </button>
+        <div className="book-component card mb-3" onClick={openModal}>
+            <div className="card-body d-flex flex-column">
+                <h3 className="book-title card-title">{book.titre}</h3>
+                <p className="book-author card-text">{book.auteur}</p>
+                <p className="book-state card-text">État: {book.etat}</p> {/* Affichage de l'état du livre */}
+                {book.image && <img className="book-image card-img-top" src={book.image} alt={`${book.titre} cover`} />}
+                <button className="favorite-button btn btn-primary mt-auto" onClick={(e) => { e.stopPropagation(); onFavoriteToggle(book) }}>
+                    {book.favori ? "Retirer des favoris" : "Ajouter aux favoris"}
+                </button>
+            </div>
             <Modal 
                 isOpen={modalIsOpen} 
                 onRequestClose={closeModal} 
                 contentLabel="Book Details Modal"
             >
-                <h2 className="modal-title">{book.titre}</h2>
-                <p className="modal-author">Auteur: {book.auteur}</p>
-                <p className="modal-pages">Nombre de pages: {book.nombre_de_pages}</p>
-                <form onSubmit={handleFormSubmit}>
-                    <label>
-                        État:
-                        <select value={newEtat} onChange={handleEtatChange}>
-                            <option value="à lire">À lire</option>
-                            <option value="en cours de lecture">En cours de lecture</option>
-                            <option value="fini">Fini</option>
-                        </select>
-                    </label>
-                    {newEtat === "en cours de lecture" && (
-                        <>
-                            <label>
-                                Dernière page lue:
-                                <input
-                                    type="number"
-                                    value={lastPageRead}
-                                    onChange={handlePageReadChange}
-                                    min="0"
-                                    max={book.nombre_de_pages}
-                                />
-                            </label>
-                            <div>
-                                Progression:
-                                <progress value={lastPageRead} max={book.nombre_de_pages}>
-                                    {((lastPageRead / book.nombre_de_pages) * 100).toFixed(2)}%
-                                </progress>
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">{book.titre}</h5>
+                        <button type="button" className="close" onClick={closeModal}>
+                            <span>&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <p>Auteur: {book.auteur}</p>
+                        <p>Nombre de pages: {book.nombre_de_pages}</p>
+                        <form onSubmit={handleFormSubmit}>
+                            <div className="form-group">
+                                <div>
+                                <label>État:</label>
+                                <select className="form-control" value={newEtat} onChange={handleEtatChange}>
+                                    <option value="À lire">À lire</option> 
+                                    <option value="En cours de lecture">En cours de lecture</option>
+                                    <option value="Fini">Fini</option>
+                                </select>
+                                </div>
                             </div>
-                        </>
-                    )}
-                    <button type="submit">Mettre à jour</button>
-                </form>
-                <button onClick={(e) => closeModal(e)}>Fermer</button>
+                            {newEtat === "En cours de lecture" && (
+                                <div className="form-group">
+                                    <label>Dernière page lue:</label>
+                                    <input
+                                        type="number"
+                                        className="form-control"
+                                        value={lastPageRead}
+                                        onChange={handlePageReadChange}
+                                        min="0"
+                                        max={book.nombre_de_pages}
+                                    />
+                                    <div className="progress mt-2">
+                                        <div 
+                                            className="progress-bar" 
+                                            role="progressbar" 
+                                            style={{ width: `${((lastPageRead / book.nombre_de_pages) * 100).toFixed(2)}%` }}
+                                            aria-valuenow={lastPageRead} 
+                                            aria-valuemin="0" 
+                                            aria-valuemax={book.nombre_de_pages}
+                                        >
+                                            {((lastPageRead / book.nombre_de_pages) * 100).toFixed(2)}%
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                            <button type="submit" className="btn btn-success">Mettre à jour</button>
+                        </form>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" onClick={closeModal}>Fermer</button>
+                    </div>
+                </div>
             </Modal>
         </div>
     );
--- front/src/components/nav.jsx
+++ front/src/components/nav.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import logobook from '../logobook.png';
 function Nav() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Accueil</Link>
+ <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        <img src={logobook} alt="Book Buddy Logo" width="30" height="30" className="d-inline-block align-top" />
+        {' '}
+        Book Buddy
+      </Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Accueil</Link>
           </li>
-          <li>
-            <Link to="/collections">Collections</Link>
+          <li className="nav-item">
+            <Link className="nav-link" to="/collections">Collections</Link>
           </li>
-          <li>
-            <Link to="/profil">Profil</Link>
+          <li className="nav-item">
+            <Link className="nav-link" to="/profil">Profil</Link>
           </li>
         </ul>
-      </nav>
+      </div>
+    </nav>
     </>
   );
 }
