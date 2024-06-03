a56f5863b626c5d401410726db8a1c75b8d34bf3
aaliyah-houidef <aaliyah.houidef@laplateforme.io>
2024-06-03 18:40:34 +0200

    push des dernieres modifs apportées

--- front/src/components/Forms/FormBook.jsx
+++ front/src/components/Forms/FormBook.jsx
@@ -1,15 +1,45 @@
 import React, { useState } from "react";
 
-function FormBook(){
+function FormBook() {
     const [titre, setTitre] = useState("");
     const [auteur, setAuteur] = useState("");
     const [image, setImage] = useState("");
     const [page, setPage] = useState("");
     const [selectedEtat, setSelectedEtat] = useState("lire");
     const [genre, setGenre] = useState("");
+    const token = localStorage.getItem('token'); 
 
-    const handleSubmit = (e) => {
+    const isValidImageUrl = (url) => {
+        const imageUrlPattern = /\.(jpeg|jpg|gif|png|bmp)$/;
+
+        return imageUrlPattern.test(url);
+    };
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
+
+        if (titre.length < 3) {
+            alert("Le titre doit comporter au moins 3 caractères.");
+            return;
+        }
+
+        if (!/^[A-Za-z\s]{3,}$/.test(auteur)) {
+            alert("L'auteur doit comporter au moins 3 caractères.");
+            return;
+        }
+
+        // Vérifie si l'URL de l'image est valide
+        if (!isValidImageUrl(image)) {
+            alert("Veuillez entrer une URL d'image valide.");
+            return;
+        }
+
+
+        if (!/^[A-Za-z\s]{3,}$/.test(genre)) {
+            alert("Le genre doit comporter au moins 3 caractères.");
+            return;
+        }
+                
         //validation et conversion de page
         const pageAsNumber = Number(page);
         if (isNaN(pageAsNumber) || pageAsNumber <= 0) {
@@ -26,11 +56,31 @@ function FormBook(){
             etat: selectedEtat,
             genre,
         };
+        console.log("Données envoyées:", newBook);
 
-        // Vous pouvez maintenant envoyer `newBook` à votre backend ou faire ce que vous voulez avec.
-        console.log(newBook);
+        try {
+            const response = await fetch('http://localhost:3000/api/books/addBook', {
+                method: "POST",
+                headers: {
+                    "Authorization": "Bearer " + token,
+                    "Content-Type": "application/json" // Indiquez que le contenu est JSON
+                },
+                body: JSON.stringify(newBook) // Convertissez l'objet en JSON
+            });
+        
+            if (response.ok) {
+            const addedBook = await response.json();
+            console.log("Livre ajouté avec succès:", addedBook);
+            alert("Livre ajouté avec succès !");
+        } else {
+            console.error("Échec de l'ajout du livre");
+            alert("Échec de l'ajout du livre");
+        }
+    } catch (error) {
+        console.error("Erreur lors de l'ajout du livre:", error);
+        alert("Erreur lors de l'ajout du livre");
+    }
 
-        //réinitialise le formulaire
         setTitre("");
         setAuteur("");
         setImage("");
@@ -38,7 +88,11 @@ function FormBook(){
         setSelectedEtat("lire");
         setGenre("");
 
-        // script pour gerer les images
+    };
+
+    const handleImageChange = (e) => {
+        const url = e.target.value;
+        setImage(url);
     };
 
     return(
@@ -52,8 +106,8 @@ function FormBook(){
             <input value={auteur} onChange={e => setAuteur(e.target.value)} />
         </label>
         <label>
-            Image:
-            <input type="file" value={image} onChange={e => setImage(e.target.value)} />
+            Image (URL):
+            <input value={image} onChange={e => setImage(e.target.value)} type="text" />
         </label>
         <label>
             État de lecture:
